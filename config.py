21c9e40a85a47078110563b576f29fe2f7177514
thelivezoneuz <71556586+thelivezoneuz@users.noreply.github.com>
2021-10-27 14:47:23 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = '2069507048:AAEbU44okPlB7_LqHqmwnMgjU59JbVQocA0'
+APP_ID = '1663652'
+API_HASH = 'a37d090c554e5593350556985bd9dc05'
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
